6ae5649b70d75dc4fceea95da5cc8cd7fc956a39
eelcire <eelcire.eelcire@hotmail.com>
2020-01-07 17:24:36 -0600

    mui & post card details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import MuiThemeProvidor from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 import home from './pages/home'
 import login from './pages/login'
@@ -8,20 +10,42 @@ import signup from './pages/signup'
 
 import NavBar from './components/Navbar'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <NavBar></NavBar>
-        <div className = "container">
-          <Switch>
-            <Route exact path = "/" component = {home} />
-            <Route exact path = "/login" component = {login} />
-            <Route exact path = "/signup" component = {signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvidor theme = {theme}>
+      <div className="App">
+        <Router>
+          <NavBar></NavBar>
+          <div className = "container">
+            <Switch>
+              <Route exact path = "/" component = {home} />
+              <Route exact path = "/login" component = {login} />
+              <Route exact path = "/signup" component = {signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvidor>
   );
 }
 
